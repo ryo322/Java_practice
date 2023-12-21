40e5c54b4dc3c89e142c428a60c19d437bf76f96
ryo322 <syaro12@icloud.com>
2023-12-21 16:30:19 +0900

    コマンドライン

--- Main.java
+++ Main.java
@@ -1,21 +1,7 @@
 class Main {
     public static void main(String[] args) {
-        int[][] allScore = new int[2][3];
-        allScore[0][0] = 80;
-        allScore[0][1] = 100;
-        allScore[0][2] = 70;
-        allScore[1][0] = 85;
-        allScore[1][1] = 95;
-        allScore[1][2] = 80;
-
-        System.out.println(allScore[0][0] + "点");
-        System.out.println(allScore[0][1] + "点");
-        System.out.println(allScore[0][2] + "点");
-        System.out.println(allScore[1][0] + "点");
-        System.out.println(allScore[1][1] + "点");
-        System.out.println(allScore[1][2] + "点");
-
-        System.out.println("allScore.length：" + allScore.length);
-        System.out.println("allScore[0].length：" + allScore[0].length);
+      System.out.println("args[0]:" + args[0]);
+      System.out.println("args[1]:" + args[1]);
+      System.out.println("args.length:" + args.length);
     }
 }
