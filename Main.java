a786c5a85e7236f61919dc4c454a9e0a8b310fe0
ryo322 <syaro12@icloud.com>
2023-12-21 14:29:29 +0900

    変数

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 class Main {
     public static void main(String[] args) {
-        String msg = "HelloWorld!";
-        System.out.println(msg);
+       int month;
+       month = 12;
+       int day = 21;
+
+       System.out.println("このコードは");
+       System.out.println(month + "月" + day + "日に書きました");
     }
 }
