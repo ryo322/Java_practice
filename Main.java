525589ee28949f052bd52ff14088c3ccf2f4733f
ryo322 <syaro12@icloud.com>
2023-12-22 15:25:49 +0900

    演習問題

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 class Main {
-    public static void main(String[] args) {
-      System.out.println("args[0]:" + args[0]);
-      System.out.println("args[1]:" + args[1]);
-      System.out.println("args.length:" + args.length);
-    }
+   public static void main(String[] args){
+     System.out.println("こんにちは");
+     System.out.println("頑張ろう");
+   }
 }
