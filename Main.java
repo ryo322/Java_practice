a43f4ea3458c8336aa7a18c9a87c4df57ce89cd5
ryo322 <syaro12@icloud.com>
2023-12-22 15:48:18 +0900

    演習問題２−４

--- Main.java
+++ Main.java
@@ -1,17 +1,15 @@
 class Main {
    public static void main(String[] args){
-     String[] name = new String[5];
+     int[] data = new int[4];
 
-     name[0] = "櫻井";
-     name[1] = "田原";
-     name[2] = "中川";
-     name[3] = "鈴木";
-     name[4] = "佐藤";
+     data[0] = 90;
+     data[1] = 80;
+     data[2] = 100;
+     data[3] = 90;
 
-     System.out.println(name[0]);
-     System.out.println(name[1]);
-     System.out.println(name[2]);
-     System.out.println(name[3]);
-     System.out.println(name[4]);
+     System.out.println("１人目は" + data[0] + "点");
+     System.out.println("２人目は" + data[1] + "点");
+     System.out.println("３人目は" + data[2] + "点");
+     System.out.println("４人目は" + data[3] + "点");
    }
 }
