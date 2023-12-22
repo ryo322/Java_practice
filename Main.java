8e3c6b6fceb32252b86b834ece2b182e31cd2c54
ryo322 <syaro12@icloud.com>
2023-12-22 15:40:32 +0900

    演習問題２−３

--- Main.java
+++ Main.java
@@ -1,15 +1,17 @@
 class Main {
    public static void main(String[] args){
-     int num = 10;
-     double pi = 3.14;
-     char blood = 'A';
-     boolean fun = true;
-     String msg = "頑張ろう";
+     String[] name = new String[5];
 
-     System.out.println(num);
-     System.out.println(pi);
-     System.out.println(blood);
-     System.out.println(fun);
-     System.out.println(msg);
+     name[0] = "櫻井";
+     name[1] = "田原";
+     name[2] = "中川";
+     name[3] = "鈴木";
+     name[4] = "佐藤";
+
+     System.out.println(name[0]);
+     System.out.println(name[1]);
+     System.out.println(name[2]);
+     System.out.println(name[3]);
+     System.out.println(name[4]);
    }
 }
