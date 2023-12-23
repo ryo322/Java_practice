33ded951cdf92d84a104d9994ec144484f031b73
ryo322 <syaro12@icloud.com>
2023-12-23 16:22:42 +0900

    演習問題２−１捕捉

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ class Main {
     int width = Integer.parseInt(args[1]);
     int area;
 
-    area = height * width / 2;
+    area = height * width / 2;　//2.0とすればdouble型となり面積も小数で表示できる（int areaをdouble areaに変える)
 
     System.out.println("高さ：" + height);
     System.out.println("幅：" + width);
