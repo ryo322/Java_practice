eb138174f5c881058246a8ac1c97331caaf631bc
ryo322 <syaro12@icloud.com>
2023-12-23 16:02:34 +0900

    while文

--- Main.java
+++ Main.java
@@ -1,18 +1,9 @@
 class Main {
    public static void main(String[] args){
-    int num = Integer.parseInt(args[0]);
-    switch (num){
-      case 1: //セミコロンではなくコロン
-       System.out.println("入園料金8400円");
-       break;  //break;で処理を抜ける
-      case 2:
-       System.out.println("入園料金7000円");
-       break;
-      case 3:
-       System.out.println("入園料金5000円");
-       break;
-      default: //if文におけるelseのようなもの
-      System.out.println("１：一般、２：中・高校生、３：小学生・幼児");
+    int i = 0;
+    while( i < 3 ) {
+      System.out.println("i =" + i);
+      i++;
     }
-   }
+  }
 }
