efb25477eda9e1f87baae4d9d889ce30134035be
ryo322 <syaro12@icloud.com>
2023-12-26 15:51:46 +0900

    演習問題４−５−１

--- Main.java
+++ Main.java
@@ -1,40 +1,37 @@
- class Person {
-  private String name;
+ class Car1 {
+  private int no;
 
-  public Person(String name) {
-    this.name = name;
+  public void setNo(int n) {
+    no = n;
   }
 
   public void display() {
-    System.out.println("名前：" + name);
+    System.out.println("ナンバーは" + no + "です");
   }
 }
 
- final class Student extends Person {
-  private int stuNo;
+ class Taxi1 extends Car1 {
+  private int price;
 
-  public Student(String name, int stuNo) {
-    super(name); 
-    this.stuNo = stuNo;
+  public void start() {
+    price = 420;
+  }
+
+  public void run(){
+    price = price + 80;
   }
 
   public void display() {
-    super.display();
-    System.out.println("学籍番号:" + stuNo);
+    System.out.println("料金は" + price + "円");
   }
 }
 
-class  Stusample {
+class Drive1 {
   public static void main(String[] args){
-    final int BASE_NO = 1000;
-    Student stu1 = new Student("田中",BASE_NO+1);
-    stu1.display();
-    Student stu2 = new Student("佐藤",BASE_NO+2);
-    stu2.display();
+    Taxi1 t1 = new Taxi1();
+    t1.setNo(2525);
+    t1.start();
+    t1.run();
+    t1.display();
   }
 }
-
-//finalキーワードはクラス、メンバ変数、メソッドにつけられる修飾子。”これで最後”という意味
-//[クラス]継承ができなくなる
-//[メソッド]オーバーライドができなくなる
-//[メンバ変数]変数への代入ができなくなる→定数を作れる(定数名はすべて大文字)
