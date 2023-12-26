70a6b17e275d3461385fe2f56401120366398287
ryo322 <syaro12@icloud.com>
2023-12-26 15:37:09 +0900

    final

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
   private String name;
 
   public Person(String name) {
-    this.name = name; //このthis.は二行目のnameを指している
+    this.name = name;
   }
 
   public void display() {
@@ -10,15 +10,11 @@
   }
 }
 
- class Student extends Person {
+ final class Student extends Person {
   private int stuNo;
 
-  public Student(String name) {
-    this(name, 999); //this()はコンストラクタの呼び出し
-  }
-
   public Student(String name, int stuNo) {
-    super(name); //superはスーパークラスの呼び出し
+    super(name); 
     this.stuNo = stuNo;
   }
 
@@ -30,7 +26,15 @@
 
 class  Stusample {
   public static void main(String[] args){
-    Student stu = new Student("田中");
-    stu.display();
+    final int BASE_NO = 1000;
+    Student stu1 = new Student("田中",BASE_NO+1);
+    stu1.display();
+    Student stu2 = new Student("佐藤",BASE_NO+2);
+    stu2.display();
   }
 }
+
+//finalキーワードはクラス、メンバ変数、メソッドにつけられる修飾子。”これで最後”という意味
+//[クラス]継承ができなくなる
+//[メソッド]オーバーライドができなくなる
+//[メンバ変数]変数への代入ができなくなる→定数を作れる(定数名はすべて大文字)
