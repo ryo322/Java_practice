1121bb87beecc915c5e61f9bf8d22bc4f70c50c6
ryo322 <syaro12@icloud.com>
2023-12-26 15:59:24 +0900

    演習問題４−５−２

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
  class Car1 {
   private int no;
 
-  public void setNo(int n) {
-    no = n;
+  public Car1(int no) {
+    this.no = no;
   }
 
   public void display() {
@@ -13,6 +13,15 @@
  class Taxi1 extends Car1 {
   private int price;
 
+  public Taxi1(int no){
+    this(no, 0);
+  }
+
+  public Taxi1(int no, int price){
+    super(no);
+    this.price = price;
+  }
+
   public void start() {
     price = 420;
   }
@@ -22,14 +31,14 @@
   }
 
   public void display() {
+    super.display();
     System.out.println("料金は" + price + "円");
   }
 }
 
 class Drive1 {
   public static void main(String[] args){
-    Taxi1 t1 = new Taxi1();
-    t1.setNo(2525);
+    Taxi1 t1 = new Taxi1(2525);
     t1.start();
     t1.run();
     t1.display();
