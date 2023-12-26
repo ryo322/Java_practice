a0a8c5940c8d667dce1272bdd44c4dd4f300c480
ryo322 <syaro12@icloud.com>
2023-12-26 13:24:24 +0900

    オーバーライド

--- Main.java
+++ Main.java
@@ -4,9 +4,12 @@
   public void setName(String n) {
     name = n;
   }
+  public String getName() {
+    return name;
+  }
 
   public void display() {
-    System.out.println("名前" + name);
+    System.out.println("名前：" + name);
   }
 }
 
@@ -17,7 +20,8 @@
     stuNo = s;
   }
 
-  public void displayStuNo() {
+  public void display() {
+    System.out.println("名前：" + getName());
     System.out.println("学籍番号" + stuNo);
   }
 }
@@ -27,14 +31,10 @@ class  Stusample {
     Student stu = new Student();
 
     stu.setName("田中");
-    stu.display();
-
     stu.setStuNo(1);
-    stu.displayStuNo();
+    stu.display();
   }
 }
 
-//継承とは、既存のクラスをもとに変数やメソッドを追加したクラスを作ること
-//継承関係にある親のクラスをスーパークラス、子のクラスをサブクラスという
-//継承を使うメリットはコーディング、メンテナンスが楽
-//継承できるクラスは１つだけであるのと、コンストラクタは継承しない
+//サブクラスでスーパークラスのメソッドを再定義することをオーバーライドという
+//オーバーライドの条件は、戻り値の型、メソッド名、引数の型と数がスーパークラスとサブクラスで同じであること
