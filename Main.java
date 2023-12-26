a43bd3570decba40779e7148d54f9a938c544d85
ryo322 <syaro12@icloud.com>
2023-12-26 15:25:41 +0900

    thisとsuper

--- Main.java
+++ Main.java
@@ -1,11 +1,8 @@
  class Person {
   private String name;
 
-  public void setName(String n) {
-    name = n;
-  }
-  public String getName() {
-    return name;
+  public Person(String name) {
+    this.name = name; //このthis.は二行目のnameを指している
   }
 
   public void display() {
@@ -16,25 +13,24 @@
  class Student extends Person {
   private int stuNo;
 
-  public void setStuNo(int s) {
-    stuNo = s;
+  public Student(String name) {
+    this(name, 999); //this()はコンストラクタの呼び出し
+  }
+
+  public Student(String name, int stuNo) {
+    super(name); //superはスーパークラスの呼び出し
+    this.stuNo = stuNo;
   }
 
   public void display() {
-    System.out.println("名前：" + getName());
-    System.out.println("学籍番号" + stuNo);
+    super.display();
+    System.out.println("学籍番号:" + stuNo);
   }
 }
 
 class  Stusample {
   public static void main(String[] args){
-    Student stu = new Student();
-
-    stu.setName("田中");
-    stu.setStuNo(1);
+    Student stu = new Student("田中");
     stu.display();
   }
 }
-
-//サブクラスでスーパークラスのメソッドを再定義することをオーバーライドという
-//オーバーライドの条件は、戻り値の型、メソッド名、引数の型と数がスーパークラスとサブクラスで同じであること
