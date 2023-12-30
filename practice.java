cc940acc529e0a2c401d3f33a83bf8e4e614ffa1
ryo322 <syaro12@icloud.com>
2023-12-30 13:12:28 +0900

    練習問題２

--- practice.java
+++ practice.java
@@ -1,57 +1,39 @@
-public class practice {
-    public static void main(String[] args) {
-        //1-1
-        System.out.println("Hello World");
-        //1-2
-        int a = 11;
-        System.out.println("a=" + a);
-        //1-3
-        int b = 13;
-        int c = 17;
-        System.out.println("b+c=" + (b + c));
-        //1-4
-        int d = 13 + 17;
-        System.out.println("d=" + d);
-        //1-5
-        System.out.println("13+17=" + (13 + 17));
-        //1-6
-        int e = 7;
-        e = e * 3;
-        System.out.println("e=" + e);
-        e = e / 2;
-        System.out.println("e=" + e);
-        //1-7
-        int f = 3;
-        int g = 7;
-        int h = f + g;
+import java.io.*;
 
-        f = h - f;
-        g = h - g;
-        System.out.println("f=" + f + ",g=" + g);
-        //1-8
-        int i = 19;
-        int j = 23;
-        int k = i * j;
-        System.out.println("k=" + k);
-        //1-9
-        int l = 10;
-        System.out.println("l=" + (l * 2));
-        System.out.println("l=" + (l * 3));
-        System.out.println("l=" + (l * 4));
-        //1-10
-        int m = 10;
-        System.out.println("m=" + (m * 1));
-        System.out.println("m=" + (m * m));
-        System.out.println("m=" + (m * m * m));
-        //1-11
-        int n = 10;
-        System.out.println(n + "÷３＝" + (n / 3) + "あまり" + (n % 3));
-        //1-12
-        int o = 10;
-        o++;
-        System.out.println(o);
-        o--;
-        o--;
-        System.out.println(o);
+public class practice {
+    public static void main( String[] args ) throws IOException {
+        //2-1
+        BufferedReader br = new BufferedReader(
+                                new InputStreamReader( System.in ) );
+
+        String  s = br.readLine();
+        System.out.println( s );
+        //2-2
+        int a = Integer.parseInt( br.readLine() );
+        System.out.println( a );
+        //2-3
+        int b = Integer.parseInt( br.readLine() );
+        System.out.println( b );
+        System.out.println( b * b );
+        System.out.println( b * b * b );
+        //2-4
+        int c = Integer.parseInt( br.readLine() );
+        int d = Integer.parseInt( br.readLine() );
+
+        System.out.println("和 " + ( c + d ) );
+        System.out.println("差 " + ( c - d ) );
+        System.out.println("積 " + ( c * d ) );
+        System.out.println("商 " + ( c / d ) );
+        System.out.println("余り " + ( c % d ) );
+        //2-5
+        int e = Integer.parseInt( br.readLine() );
+        int f = Integer.parseInt( br.readLine() );
+
+        System.out.println( "平均値=" + ( ( e + f ) / 2 ) );
+        //2-6
+        System.out.println( "あなたの年齢は？" );
+        int age = Integer.parseInt( br.readLine() );
+
+        System.out.println( "生まれてから今まで、およそ " + ( age * 365 ) + "日です" );
     }
 }
