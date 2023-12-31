ebb6df702f20cace8408e7525b7c313abc1794d9
ryo322 <syaro12@icloud.com>
2023-12-31 17:35:17 +0900

    練習問題４

--- practice.java
+++ practice.java
@@ -96,6 +96,146 @@ public class practice {
 
         for( int i = 0 ; i < count ; i++ )
             System.out.print( "*" );
-        
+        //4-11
+        int count = Integer.parseInt( br.readLine() );
+
+        for( int i = 0, out = 0 ; i < count ; i++ ) {
+            System.out.print( out++ );
+
+            if( out > 9 )
+             out = 0;
+        }
+        //4-12
+        int sum = 0;
+
+        while( sum <= 100 )
+            sum += Integer.parseInt( br.readLine() );
+
+        System.out.println( "合計は " + sum );
+        //4-13
+        int strike = 0;
+        int ball = 0;
+
+        while( strike < 3 && ball < 4 ) {
+            System.out.print( "ストライク=1 or ボール=2 ？" );
+
+            int judge = Integer.parseInt( br.readLine() );
+
+            if( judge == 1 )
+                strike++;
+            else if( judge == 2 )
+                ball++;
+        }
+
+        System.out.println( ball + "ボール," + strike + "ストライク" );
+        //4-14
+        int strike = 0;
+        int ball = 0;
+
+        while( strike < 3 && ball < 4 ) {
+            System.out.print( "ストライク=1 or ボール=2 or ファウル=3？" );
+
+            int judge = Integer.parseInt( br.readLine() );
+
+            if( judge == 1 )
+                strike++;
+            else if( judge == 2 )
+                ball++;
+            else if( judge == 3 && strike < 2 )
+                strike++;
+        }
+
+        System.out.println( ball + "ボール," + strike + "ストライク" );
+        //4-15
+        int value = Integer.parseInt( br.readLine() );
+        int n;
+
+        for( n = 2 ; n <= ( value / 2 ) ; n++ ){
+            if( ( value % n ) == 0 )
+             break;
+        }
+
+        if( n > ( value / 2 ) )
+            System.out.println( value + "は素数です" );
+        else
+            System.out.println( value + "は素数ではありません" );
+        //4-16
+        int n = Integer.parseInt( br.readLine() );
+
+        for( int x = 2 ; n > 1 ; x++ ){
+            while( ( n % x ) == 0 ){
+                System.out.print( x + " " );
+                n /= x;
+            }
+        }
+        //4-17
+        for( int p = 1 ; p <= 9 ; p++ ) {
+            for( int q = 1 ; q <= 9 ; q++ )
+                System.out.printf(" %2d", p * q );
+
+         System.out.println();
+        }
+        //4-18
+        int sum = 0;
+
+        for( ; ; ) {
+            int value = Integer.parseInt( br.readLine() );
+
+         if( value == 0 )
+             break;
+
+            sum += value;
+        }
+
+        System.out.println( "合計は" + sum );
+        //4-19
+        int sum = 0;
+        int count = 0;
+
+        for( ; ; ) {
+            int value = Integer.parseInt( br.readLine() );
+
+            if( value == 0 )
+             break;
+
+            sum += value;
+            count++;
+        }
+
+        System.out.println( "平均は" + ( sum / count ) );
+        //4-20
+        int size = Integer.parseInt( br.readLine() );
+
+        for( int i = 0 ; i < size ; i++ ) {
+            for( int j = 0 ; j <= i ; j++ )
+            System.out.print( "$" );
+
+            System.out.println();
+        }
+        //4-21
+        int size = Integer.parseInt( br.readLine() );
+
+        for( int i = 0 ; i < size ; i++ ) {
+            for( int j = 0 ; j < size ; j++ ) {
+                if( i == j || ( size - i - 1 ) == j )
+                    System.out.print( "X" );
+                else
+                    System.out.print( " " );
+            }
+
+            System.out.println( "" );
+        }
+        //4-22
+        int     n1 = 0;
+        int     n2 = 1;
+        int     n3 = n1 + n2;
+
+        while( n1 <= 1000 ) {
+            System.out.print( n1 + "," );
+
+            n1 = n2;
+            n2 = n3;
+            n3 = n1 + n2;
+        }   
     }
 }
