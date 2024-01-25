20e3451569d4a4204c8b334b1eca1eb29fcc8aa2
ryo322 <syaro12@icloud.com>
2024-01-25 10:13:04 +0900

    progate

--- practice.java
+++ practice.java
@@ -1,10 +1,13 @@
 class Main {
     public static void main(String[] args) {
+      // 数値の17を出力してください
+      System.out.println(17);
       
-      //この文章をコメントにしてください
+      // 5に3を足した値を出力してください
+      System.out.println(5 + 3);
       
-      // 「こんにちは、Java」と出力してください
-        System.out.println("こんにちは、Java");
+      // 「5 + 3」を文字列として出力してください
+      System.out.println("5 + 3");
       
     }
   }
