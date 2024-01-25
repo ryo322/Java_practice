e0565cd170f2b699d0f9f8e84488c4361b0ebf58
ryo322 <syaro12@icloud.com>
2024-01-25 10:11:11 +0900

    progate

--- practice.java
+++ practice.java
@@ -1,6 +1,10 @@
-class Main{
-    public static void main(String[] args){
-        System.out.println("Hello Java");
-    }    
-}
-
+class Main {
+    public static void main(String[] args) {
+      
+      //この文章をコメントにしてください
+      
+      // 「こんにちは、Java」と出力してください
+        System.out.println("こんにちは、Java");
+      
+    }
+  }
