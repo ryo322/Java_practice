f3154f65b1ac544948d2207fab7793a945ad4899
ryo322 <syaro12@icloud.com>
2024-01-25 10:30:02 +0900

    progate

--- practice.java
+++ practice.java
@@ -1,14 +1,18 @@
 class Main {
     public static void main(String[] args) {
-      int number = 3;
-      System.out.println(number);
+      int number = 8;
       
-      // 変数numberの値に7を足して、変数numberを上書きしてください
-      number = number + 7;
+      // 変数numberに7をかけて、変数numberを上書きしてください
+      number *= 7;
       
       // 変数numberを出力してください
       System.out.println(number);
-        
+      
+      // 変数numberの値に1を足して、変数numberを上書きしてください
+      number ++;
+      
+      // 変数numberを出力してください
+      System.out.println(number);
+      
     }
   }
-  
