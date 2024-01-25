9107c501718a5e2089c05825e04157f8a82e2041
ryo322 <syaro12@icloud.com>
2024-01-25 10:27:56 +0900

    progate

--- practice.java
+++ practice.java
@@ -1,22 +1,14 @@
 class Main {
     public static void main(String[] args) {
-      int number = 11;
-      String text = "Ruby";
+      int number = 3;
       System.out.println(number);
-      System.out.println(text);
       
-      // 変数numberを9で上書きしてください
-      number = 9;
+      // 変数numberの値に7を足して、変数numberを上書きしてください
+      number = number + 7;
       
       // 変数numberを出力してください
       System.out.println(number);
-      
-      // 変数textを「Java」で上書きしてください
-      text = "Java";
-      
-      // 変数textを出力してください
-      System.out.println(text);
-      
+        
     }
   }
   
