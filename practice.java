726620df149c55c2a870b48ac8ade59b657569c9
ryo322 <syaro12@icloud.com>
2024-01-25 10:17:07 +0900

    progate

--- practice.java
+++ practice.java
@@ -1,13 +1,14 @@
 class Main {
     public static void main(String[] args) {
-      // 12を3で割った値を出力してください
-      System.out.println(12 / 3);
+      // "こんにちは"と"世界"を連結して出力してください
+      System.out.println("こんにちは" + "世界");
       
-      // 3に6を掛けた値を出力してください
-      System.out.println(3 * 6);
+      // "38"と"19"を連結して出力してください
+      System.out.println("38" + "19");
       
-      // 8を3で割った時の余りを出力してください
-      System.out.println(8 % 3);
+      // 38と19を足して出力してください
+      System.out.println(38 + 19);
+      
       
     }
   }
