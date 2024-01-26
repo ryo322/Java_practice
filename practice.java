080059824c8fb4a0d099d5066d1feeb218d6ed26
ryo322 <syaro12@icloud.com>
2024-01-26 10:37:39 +0900

    progate

--- practice.java
+++ practice.java
@@ -1,17 +1,24 @@
 class Main {
-    public static void main(String[] args) {
-      // double型の変数number1を定義し、8.5を代入してください。
-      double number1 = 8.5;
-      
-      // double型の変数number2を定義し、3.4を代入してください。
-      double number2 = 3.4;
-      
-      // number1にnumber2を足した値を出力してください
-      System.out.println(number1 + number2);
-      
-      // number1からnumber2を引いた値を出力してください
-      System.out.println(number1 - number2);
-      
-    }
+  public static void main(String[] args) {
+    // 「true」を用いて、「真」を表す真偽値を出力してください
+    System.out.println(true);
+    
+    // 「false」を用いて、「偽」を表す真偽値を出力してください
+    System.out.println(false);
+    
+    // 「==」を用いて、値を比較した結果を出力してください
+    System.out.println(12 / 4 == 3);
+    
+    // 「!=」を用いて、値を比較した結果を出力してください
+    System.out.println(12 / 4 != 3);
+    
+    // 変数を定義し、値を比較した結果を代入してください
+    boolean bool = 3 * 9 == 27;
+    
+    // 変数boolの値を出力してください
+    System.out.println(bool);
+    
   }
+}
+
   
