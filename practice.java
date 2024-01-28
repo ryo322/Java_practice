adbb0187eeaf44976f115432b23593963a748c29
ryo322 <syaro12@icloud.com>
2024-01-28 10:31:37 +0900

    progate

--- practice.java
+++ practice.java
@@ -1,10 +1,19 @@
 class Main {
   public static void main(String[] args) {
-    // 「4 + 2」と「6」を比較し、falseとなるようにしてください
-    System.out.println(4 + 2 > 6);
+    // trueと出力されるようにしてください
+    System.out.println(true || false);
     
-    // 「4 + 2」と「6」を比較し、trueとなるようにしてください
-    System.out.println(4 + 2 >= 6);
+    // falseと出力されるようにしてください
+    System.out.println(false && true);
+    
+    // 「8 < 5」かつ「3 >= 2」の結果を出力してください
+    System.out.println(8 < 5 && 3 >= 2);
+    
+    // 「8 < 5」または「3 >= 2」の結果を出力してください
+    System.out.println(8 < 5 || 3 >= 2);
+    
+    // 「8 < 5」でない、の結果を出力してください
+    System.out.println(!(8 < 5));
     
   }
 }
